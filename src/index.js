e33db1834ad224c610931d8eeab397f485c65bb0
franciscaserrao <francisca.s.serrao@gmail.com>
2023-11-14 18:31:42 +0000

    Updated design elements according to meteo

--- src/index.js
+++ src/index.js
@@ -25,24 +25,74 @@ function refreshWeather(response) {
   timeElement.innerHTML = formatDate(date);
 
   let iconElement = document.querySelector("#icon");
+  let backgroundElement = document.querySelector("#background");
+  let headerElement = document.querySelector("#header");
+  let footerElement = document.querySelector("#footer");
+  let formElement = document.querySelector("#search-form-input");
+  let submitElement = document.querySelector("#search-city-button");
+  let daysElement = document.querySelector("#days");
+  let linkElement = document.querySelector("#link");
+  let linkElement2 = document.querySelector("#link2");
+  let linkElement3 = document.querySelector("#link3");
+
   if (description === "clear sky") {
     iconElement.innerHTML = "<i class='fa-solid fa-circle'></i>";
+    backgroundElement.style.backgroundColor = `#eae4ca`;
+    backgroundElement.style.color = `#1b1c1e`;
+    formElement.style.backgroundColor = `#fcf5da`;
+    submitElement.style.backgroundColor = `#fcf5da`;
+    formElement.style.color = `#1b1c1e`;
+    submitElement.style.color = `#1b1c1e`;
+    daysElement.style.color = `#1b1c1e`;
+    headerElement.style.borderColor = ` #dcd6bdeb`;
+    footerElement.style.borderColor = ` #dcd6bdeb`;
+    footerElement.style.color = `#757575`;
   } else if (
     description === "few clouds" ||
     description === "scattered clouds"
   ) {
     iconElement.innerHTML = "<i class='fa-solid fa-cloud-sun'></i>";
+    backgroundElement.style.backgroundColor = `#D4D9D2`;
+    backgroundElement.style.color = `#233847`;
+    formElement.style.backgroundColor = `#f1f5ef`;
+    submitElement.style.backgroundColor = `#f1f5ef`;
+    formElement.style.color = `#233847`;
+    submitElement.style.color = `#233847`;
+    daysElement.style.color = `#233847`;
+    headerElement.style.borderColor = `#f1f5ef`;
+    footerElement.style.borderColor = `#f1f5ef`;
+    footerElement.style.color = `#233847`;
   } else if (
     description === "broken clouds" ||
     description === "overcast clouds"
   ) {
     iconElement.innerHTML = "<i class='fa-solid fa-cloud'></i>";
+    backgroundElement.style.backgroundColor = `#D4D9D2`;
+    backgroundElement.style.color = `#233847`;
+    formElement.style.backgroundColor = `#f1f5ef`;
+    submitElement.style.backgroundColor = `#f1f5ef`;
+    formElement.style.color = `#233847`;
+    submitElement.style.color = `#233847`;
+    daysElement.style.color = `#233847`;
+    headerElement.style.borderColor = `#f1f5ef`;
+    footerElement.style.borderColor = `#f1f5ef`;
+    footerElement.style.color = `#233847`;
   } else if (
     description === "mist" ||
     description === "haze" ||
     description === "fog"
   ) {
     iconElement.innerHTML = "<i class='fa-solid fa-smog'></i>";
+    backgroundElement.style.backgroundColor = `#9A5859`;
+    backgroundElement.style.color = `#D4D9D2`;
+    formElement.style.backgroundColor = `#AA6266`;
+    submitElement.style.backgroundColor = `#AA6266`;
+    formElement.style.color = `#D4D9D2`;
+    submitElement.style.color = `#D4D9D2`;
+    daysElement.style.color = `#D4D9D2`;
+    headerElement.style.borderColor = `#AA6266`;
+    footerElement.style.borderColor = `#AA6266`;
+    footerElement.style.color = `#D4D9D2`;
   } else if (
     description === "rain" ||
     description === "light rain" ||
@@ -51,13 +101,52 @@ function refreshWeather(response) {
     description === "shower rain"
   ) {
     iconElement.innerHTML = "<i class='fa-solid fa-cloud-rain'></i>";
+    backgroundElement.style.backgroundColor = `#233847`;
+    backgroundElement.style.color = `#FDEEE1`;
+    formElement.style.backgroundColor = `#2C4856`;
+    submitElement.style.backgroundColor = `#2C4856`;
+    formElement.style.color = `#FDEEE1`;
+    submitElement.style.color = `#FDEEE1`;
+    daysElement.style.color = `#FDEEE1`;
+    headerElement.style.borderColor = `#2C4856`;
+    footerElement.style.borderColor = `#2C4856`;
+    footerElement.style.color = `#FDEEE1`;
+    linkElement.style.color = `#FDEEE1`;
+    linkElement2.style.color = `#FDEEE1`;
+    linkElement3.style.color = `#FDEEE1`;
   } else if (
     description === "thunderstorm" ||
     description === "thunderstorm with light rain"
   ) {
     iconElement.innerHTML = "<i class='fa-solid fa-cloud-bolt'></i>";
-  } else if (description === "snow") {
+    backgroundElement.style.backgroundColor = `#1B1C1E`;
+    backgroundElement.style.color = `#EAE5CA`;
+    formElement.style.backgroundColor = `#292A2C`;
+    submitElement.style.backgroundColor = `#292A2C`;
+    formElement.style.color = `#EAE5CA`;
+    submitElement.style.color = `#EAE5CA`;
+    daysElement.style.color = `#EAE5CA`;
+    headerElement.style.borderColor = `#292A2C`;
+    footerElement.style.borderColor = `#292A2C`;
+    footerElement.style.color = `#EAE5CA`;
+    linkElement.style.color = `#EAE5CA`;
+    linkElement2.style.color = `#EAE5CA`;
+    linkElement3.style.color = `#EAE5CA`;
+  } else if (description === "snow" || description === "light snow") {
     iconElement.innerHTML = "<i class='fa-regular fa-snowflake'></i>";
+    backgroundElement.style.backgroundColor = `#8EAFC6`;
+    backgroundElement.style.color = `#233847`;
+    formElement.style.backgroundColor = `#9BBED2`;
+    submitElement.style.backgroundColor = `#9BBED2`;
+    formElement.style.color = `#233847`;
+    submitElement.style.color = `#233847`;
+    daysElement.style.color = `#233847`;
+    headerElement.style.borderColor = `#9BBED2`;
+    footerElement.style.borderColor = `#9BBED2`;
+    footerElement.style.color = `#233847`;
+    linkElement.style.color = `#233847`;
+    linkElement2.style.color = `#233847`;
+    linkElement3.style.color = `#233847`;
   }
 }
 
